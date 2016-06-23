7f8bb6cea9c603b6859454c65836d1a33fdd1c0d
abberik <albineriksson99@hotmail.com>
2016-06-23 16:58:08 +0200

    Temporary
    Temporary version using a semi-working pasted class.

---
+++ src/renderingsMotor/OBJLaddare.java
@@ -0,0 +1,138 @@
+package renderingsMotor;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+import org.lwjgl.util.vector.Vector2f;
+import org.lwjgl.util.vector.Vector3f;
+
+import modeller.RaaModel;
+
+public class OBJLaddare {
+
+	public static RaaModel laddaObjModel(String filnamn, Laddare laddare){
+		
+		FileReader fl = null;
+		
+		try {
+			fl = new FileReader("res/" + filnamn + ".obj");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		BufferedReader lasare = new BufferedReader(fl);
+		
+		String rad;
+		
+		ArrayList<Vector3f> verticesList = new ArrayList<Vector3f>();
+		ArrayList<Vector2f> texturesList = new ArrayList<Vector2f>();
+		ArrayList<Vector3f> normalList = new ArrayList<Vector3f>();
+		ArrayList<Integer> indices = new ArrayList<Integer>();
+		
+		float[] verticesArray = null;
+		float[] texturesArray = null;
+		float[] normalArray = null;
+		int[] indicesArray = null;
+		
+		try{
+			
+			while(true){
+				
+				rad = lasare.readLine();
+			
+				String[] inData = rad.split(" ");
+				
+				if(rad.startsWith("v ")){
+					
+					Vector3f vertex = new Vector3f(Float.parseFloat(inData[1]),Float.parseFloat(inData[2]),Float.parseFloat(inData[3]));
+					verticesList.add(vertex);
+					
+				}else if(rad.startsWith("vn ")){
+					
+					Vector3f normal = new Vector3f(Float.parseFloat(inData[1]),Float.parseFloat(inData[2]),Float.parseFloat(inData[3]));
+					normalList.add(normal);
+					
+				}else if(rad.startsWith("vn ")){
+					
+					Vector2f texture = new Vector2f(Float.parseFloat(inData[1]),Float.parseFloat(inData[2]));
+					texturesList.add(texture);
+					
+				}else if(rad.startsWith("f ")){
+					texturesArray = new float[verticesList.size() * 2];
+					normalArray = new float[verticesList.size() * 3];
+					break;
+					
+				}
+			}	
+				while(rad != null){
+					
+					if(!rad.startsWith("f ")){
+						System.out.println("Langd: " );
+						rad = lasare.readLine();
+						continue;
+					}
+					
+					String[] aktuellaRaden = rad.split(" ");
+					
+					String[] vertex1 = aktuellaRaden[1].split("/");
+					String[] vertex2 = aktuellaRaden[2].split("/");
+					String[] vertex3 = aktuellaRaden[3].split("/");
+					
+					processeraVertex(vertex1, indices, texturesList, normalList, texturesArray, normalArray);
+					processeraVertex(vertex2, indices, texturesList, normalList, texturesArray, normalArray);
+					processeraVertex(vertex3, indices, texturesList, normalList, texturesArray, normalArray);
+					
+					rad = lasare.readLine();
+					
+					
+				}
+				
+			
+			
+			lasare.close();
+			
+		}catch(Exception ex){}
+		
+		verticesArray = new float[verticesList.size() * 3];
+		indicesArray = new int[indices.size()];
+		
+		int vertexPekare = 0;
+		
+		for(Vector3f vertex : verticesList){
+			verticesArray[vertexPekare++] = vertex.x;
+			verticesArray[vertexPekare++] = vertex.y;
+			verticesArray[vertexPekare++] = vertex.z;
+			
+		}
+		
+		for(int i = 0; i < indices.size(); i++){
+			indicesArray[i] = indices.get(i);
+			
+		}
+
+		return laddare.laddaTilVAO(verticesArray, texturesArray, indicesArray);
+	}
+	
+	
+	
+	private static void processeraVertex(String[] vertexData, ArrayList<Integer> indices, ArrayList<Vector2f> texturer, ArrayList<Vector3f> normaler, float[] texturArray, float[] normalArray ) {
+		
+		int aktuellaVertexPekaren = Integer.parseInt(vertexData[0]) - 1 ;
+		indices.add(aktuellaVertexPekaren);
+		
+		Vector2f aktuellTextur = texturer.get(Integer.parseInt(vertexData[1]) - 1);
+		texturArray[aktuellaVertexPekaren * 2] = aktuellTextur.x;
+		texturArray[aktuellaVertexPekaren * 2 + 1] = 1 - aktuellTextur.y;
+		
+		Vector3f aktuellNormal = normaler.get(Integer.parseInt(vertexData[2]) - 1);
+		normalArray[aktuellaVertexPekaren * 3] = aktuellNormal.x;
+		normalArray[aktuellaVertexPekaren * 3 + 1] = aktuellNormal.y;
+		normalArray[aktuellaVertexPekaren * 3 + 2] = aktuellNormal.z;
+		
+		
+	}
+	
+}
--- src/test/SpelLoop.java
+++ src/test/SpelLoop.java
@@ -9,6 +9,8 @@ import modeller.RaaModel;
 import modeller.TextureradModell;
 import renderingsMotor.DisplayHanterare;
 import renderingsMotor.Laddare;
+import renderingsMotor.OBJLaddare;
+import renderingsMotor.OBJLoader;
 import renderingsMotor.Renderare;
 import shaders.StatiskShader;
 import texturer.ModelTextur;
@@ -22,88 +24,9 @@ public class SpelLoop {
 		Laddare laddare = new Laddare();
 		StatiskShader shader = new StatiskShader();
 		Renderare renderare = new Renderare(shader);
-
-		float[] vertices = {			
-				-0.5f,0.5f,-0.5f,	
-				-0.5f,-0.5f,-0.5f,	
-				0.5f,-0.5f,-0.5f,	
-				0.5f,0.5f,-0.5f,		
-				
-				-0.5f,0.5f,0.5f,	
-				-0.5f,-0.5f,0.5f,	
-				0.5f,-0.5f,0.5f,	
-				0.5f,0.5f,0.5f,
-				
-				0.5f,0.5f,-0.5f,	
-				0.5f,-0.5f,-0.5f,	
-				0.5f,-0.5f,0.5f,	
-				0.5f,0.5f,0.5f,
-				
-				-0.5f,0.5f,-0.5f,	
-				-0.5f,-0.5f,-0.5f,	
-				-0.5f,-0.5f,0.5f,	
-				-0.5f,0.5f,0.5f,
-				
-				-0.5f,0.5f,0.5f,
-				-0.5f,0.5f,-0.5f,
-				0.5f,0.5f,-0.5f,
-				0.5f,0.5f,0.5f,
-				
-				-0.5f,-0.5f,0.5f,
-				-0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,0.5f
-				
-		};
-		
-		float[] texturKoordinater = {
-				
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0
-
-				
-		};
-		
-		int[] indices = {
-				0,1,3,	
-				3,1,2,	
-				4,5,7,
-				7,5,6,
-				8,9,11,
-				11,9,10,
-				12,13,15,
-				15,13,14,	
-				16,17,19,
-				19,17,18,
-				20,21,23,
-				23,21,22
-
-		};
-		
-		RaaModel model = laddare.laddaTilVAO(vertices, texturKoordinater, indices);
-		ModelTextur modelTextur = new ModelTextur(laddare.laddaTextur("stenblock"));
+		RaaModel model = OBJLoader.loadObjModel("cube", laddare);
+//		RaaModel model = OBJLaddare.laddaObjModel("stall", laddare);
+		ModelTextur modelTextur = new ModelTextur(laddare.laddaTextur("cube_texture"));
 		TextureradModell texturerad_modell = new TextureradModell(model, modelTextur);
 
 		Entity entity = new Entity(texturerad_modell, new Vector3f(0, 0, -1), 0, 0, 0, 1);
