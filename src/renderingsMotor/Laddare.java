0d8bccccffe38462912ea2ce8fd9eabeb7453537
abberik <albineriksson99@hotmail.com>
2016-06-26 23:37:42 +0200

    Mipmapping

--- src/renderingsMotor/Laddare.java
+++ src/renderingsMotor/Laddare.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL14;
 import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
@@ -49,7 +50,9 @@ public class Laddare {
 		try {
 
 			textur = TextureLoader.getTexture("PNG", new FileInputStream("res/" + filNamn + ".png"));
-
+			GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
+			GL11.glTexParameteri(GL11.GL_TEXTURE_2D,GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
+			GL11.glTexParameterf(GL11.GL_TEXTURE_2D,GL14.GL_TEXTURE_LOD_BIAS, -0.4f);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
